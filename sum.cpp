34c47d3fb2b8f51b8b86f2aab97eb2837dab7fa5
okmesaksham <131997304+okmesaksham@users.noreply.github.com>
2023-05-01 12:47:33 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"enter a and b : ";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum : " <<sum;
+    return 0;
+}
