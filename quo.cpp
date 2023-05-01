235908b2ec94827ea75a1ce321088d5fabacb258
okmesaksham <131997304+okmesaksham@users.noreply.github.com>
2023-05-01 12:46:59 +0530

    Create quo.cpp

---
+++ quo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,quo,rem;
+    cout<<"Enter a and b : ";
+    cin>>a>>b;
+    quo=b/a;
+    rem=b%a;
+    cout<<"Quotient : "<<quo<<" "<<"Remainder : "<<rem;
+    return 0;
+}
