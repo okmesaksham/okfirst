cdf48bb762d374f812e635e9576f90964af24ac0
okmesaksham <131997304+okmesaksham@users.noreply.github.com>
2023-05-01 13:34:34 +0530

    Update quo.cpp

--- quo.cpp
+++ quo.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    int a,b,quo,rem;
-    cout<<"Enter a and b : ";
-    cin>>a>>b;
-    quo=b/a;
-    rem=b%a;
-    cout<<"Quotient : "<<quo<<" "<<"Remainder : "<<rem;
-    return 0;
+	int a,b,q,r;
+	cout<<"enter a and b";
+	cin>>a>>b;
+	q=a/b;
+	r=a%b;
+	cout<<q<<r;
+	return 0;
 }
