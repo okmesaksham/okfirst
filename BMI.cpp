326d9a160f736e9cad4b3c161266bbe33500df9c
okmesaksham <131997304+okmesaksham@users.noreply.github.com>
2023-05-02 13:34:38 +0530

    Create BMI.cpp

---
+++ BMI.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float weight, height,bmi;
+    cout<<"enter weight and height";
+    cin>>weight>>height;
+    bmi=weight/(height*height);
+    cout<<"bmi is :"<<bmi;
+    return 0;
+}
