149682ce4facd5bea130427c0d59bbae427d2797
okmesaksham <131997304+okmesaksham@users.noreply.github.com>
2023-05-10 22:03:36 +0530

    Create Equal or Not.cpp

---
+++ Equal or Not.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int number7, number9;
+ 
+    cout<<"Enter number7 and number9 : ";
+    cin >> number7 >> number9;
+    if (number7 == number9)
+    {
+        printf("Number7 and Number9 are equal\n");
+    }    
+    else
+    {
+        printf("Number7 and Number9 are not equal\n");
+    }
+    return 0;
+}
